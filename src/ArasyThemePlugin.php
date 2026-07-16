ba2d538a1d17d8acfcfc2f5664b7ce571fdc5a6e
rafy683 <rafy683@gmail.com>
2026-07-16 08:45:21 +0700

    refactor: remove all auth page overrides (arasy-theme no longer touches auth)

--- src/ArasyThemePlugin.php
+++ src/ArasyThemePlugin.php
@@ -9,16 +9,9 @@ use Filament\Panel;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
-use Illuminate\Contracts\Support\Htmlable;
 
 class ArasyThemePlugin implements Plugin
 {
-    protected ?string $brandName = null;
-
-    protected ?string $brandTagline = null;
-
-    protected bool $hideBrandText = false;
-
     public static function make(): static
     {
         return app(static::class);
@@ -27,42 +20,6 @@ class ArasyThemePlugin implements Plugin
     public function getId(): string
     {
         return 'arasy-theme';
-    }
-
-    public function brandName(?string $name): static
-    {
-        $this->brandName = $name;
-
-        return $this;
-    }
-
-    public function brandTagline(?string $tagline): static
-    {
-        $this->brandTagline = $tagline;
-
-        return $this;
-    }
-
-    public function hideBrandText(bool $hide = true): static
-    {
-        $this->hideBrandText = $hide;
-
-        return $this;
-    }
-
-    public function getBrandName(): ?string
-    {
-        return $this->brandName;
-    }
-
-    public function getBrandTagline(): ?string
-    {
-        return $this->brandTagline;
-    }
-
-    public function getHideBrandText(): bool
-    {
-        return $this->hideBrandText;
     }
 
     public function register(Panel $panel): void
@@ -111,45 +68,12 @@ class ArasyThemePlugin implements Plugin
                 Css::make('arasy-theme', __DIR__ . '/../resources/css/arasy.css'),
             ], 'arasy/arasy-theme');
 
-        $this->registerAuthBrandRenderHook($panel);
         $this->registerPresetStyleRenderHook($panel);
     }
 
     public function boot(Panel $panel): void
     {
         //
-    }
-
-    protected function registerAuthBrandRenderHook(Panel $panel): void
-    {
-        FilamentView::registerRenderHook(
-            PanelsRenderHook::SIMPLE_LAYOUT_START,
-            function () use ($panel): string {
-                if (Filament::getCurrentPanel()?->getId() !== $panel->getId()) {
-                    return '';
-                }
-
-                $brandName = $this->getBrandName();
-
-                if (blank($brandName)) {
-                    $panelBrandName = $panel->getBrandName();
-
-                    $brandName = $panelBrandName instanceof Htmlable
-                        ? strip_tags($panelBrandName->toHtml())
-                        : (string) $panelBrandName;
-                }
-
-                $brandLogo = $panel->getDarkModeBrandLogo() ?? $panel->getBrandLogo();
-
-                return view('arasy-theme::login-brand', [
-                    'brandName' => $brandName,
-                    'brandTagline' => $this->getBrandTagline(),
-                    'brandLogo' => $brandLogo,
-                    'brandLogoHeight' => $panel->getBrandLogoHeight(),
-                    'hideBrandText' => $this->getHideBrandText(),
-                ])->render();
-            }
-        );
     }
 
     protected function registerPresetStyleRenderHook(Panel $panel): void
